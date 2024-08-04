e5fef84e92c93c44df81fd232ecb049499adb00a
Kosei Yokoyama <koskosyokoyama@gmail.com>
2024-08-04 19:42:29 -0400

    Adding bonus challenges

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import { useState } from "react";
 
 export default function Game() {
-  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [history, setHistory] = useState([{
+    squares: Array(9).fill(null),
+    location: null
+  }]);
   const [currentMove, setCurrentMove] = useState(0);
+  const [isDescending, setIsDescending] = useState(true);
   const xIsNext = currentMove % 2 === 0;
-  const currentSquares = history[currentMove];
+  const currentSquares = history[currentMove]?.squares;
 
-  function handleMove(nextSquares) {
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+  function handleMove(nextSquares, index) {
+    const nextHistory = [...history.slice(0, currentMove + 1), {
+      squares: nextSquares,
+      location: index
+    }];
+
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
   }
@@ -16,14 +24,24 @@ export default function Game() {
     setCurrentMove(move);
   }
 
+  function toggleHistoryOrder() {
+    setIsDescending(!isDescending);
+  }
+
   const moves = history.map((h, move) => {
-    return <button key={move} onClick={() => jumpTo(move)}>{move > 0 ? `Go to move #${move}` : 'Restart game'}</button>
+    return move === currentMove ? <span key={move}>{`You are on move #${currentMove}`}</span> :
+    <button key={move} onClick={() => jumpTo(move)}>{move > 0 ? `Go to move #${move} (${Math.floor(h.location / 3) + 1}, ${(h.location % 3) + 1})` : 'Restart game'}</button>
   });
+
+  // If ascending order, reverse the history
+  if (!isDescending) {
+    moves.reverse();
+  }
 
   return <>
     <div className="game">
       <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handleMove}>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handleMove} onToggleHistoryClicked={toggleHistoryOrder}>
         </Board>
       </div>
       <div className="game-info">
@@ -33,7 +51,7 @@ export default function Game() {
   </>
 }
 
-function Board({xIsNext, squares, onPlay}) {
+function Board({xIsNext, squares, onPlay, onToggleHistoryClicked}) {
 
   function onSquareClicked(index) {
     const newSquares = squares.slice() // Create a copy of array using slice
@@ -42,7 +60,7 @@ function Board({xIsNext, squares, onPlay}) {
     }
 
     newSquares[index] = xIsNext ? 'X' : 'O';
-    onPlay(newSquares);
+    onPlay(newSquares, index);
   }
 
   function calculateWinner(squares) {
@@ -59,35 +77,38 @@ function Board({xIsNext, squares, onPlay}) {
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
+        return {
+          winner: squares[a],
+          winningSquares: [a, b, c]
+        };
       }
     }
     return null;
   }
 
   const winner = calculateWinner(squares);
-  const status = winner ? `${winner} wins` : `Next player: ${xIsNext ? 'X' : 'O'}`;
+  const winningSquares = winner?.winningSquares ?? [];
+  const isDraw = squares.findIndex(square => square === null) === -1 && !winner;
+  const status = winner ? `${winner.winner} wins` : isDraw ? `It's a draw!` : `Next player: ${xIsNext ? 'X' : 'O'}`;
+
+  const board = Array(3).fill(null).map((_, row) => {
+    const boardRow = Array(3).fill(null).map((_, col) => {
+      const index = row * 3 + col;
+      return <Square value={squares[index]} highlight={winningSquares.findIndex((square) => square === index) !== -1} onSquareClicked={() => onSquareClicked(index)}></Square>
+    });  
+
+    return <div className="board-row">
+      {boardRow}
+    </div>
+  })
 
   return <>
     <div className="status">{status}</div>
-    <div className="board-row">
-        <Square value={squares[0]} onSquareClicked={() => onSquareClicked(0)}></Square>
-        <Square value={squares[1]} onSquareClicked={() => onSquareClicked(1)}></Square>
-        <Square value={squares[2]} onSquareClicked={() => onSquareClicked(2)}></Square>
-    </div>
-    <div className="board-row">
-        <Square value={squares[3]} onSquareClicked={() => onSquareClicked(3)}></Square>
-        <Square value={squares[4]} onSquareClicked={() => onSquareClicked(4)}></Square>
-        <Square value={squares[5]} onSquareClicked={() => onSquareClicked(5)}></Square>
-    </div>
-    <div className="board-row">
-        <Square value={squares[6]} onSquareClicked={() => onSquareClicked(6)}></Square>
-        <Square value={squares[7]} onSquareClicked={() => onSquareClicked(7)}></Square>
-        <Square value={squares[8]} onSquareClicked={() => onSquareClicked(8)}></Square>
-    </div>
+    {board}
+    <button className="history-button" onClick={onToggleHistoryClicked}>Toggle History Order</button>
   </>;
 }
 
-function Square({value, onSquareClicked}) {
-  return <button className="square" onClick={onSquareClicked}>{value}</button>
+function Square({value, highlight, onSquareClicked}) {
+  return <button className={`square ${highlight ? 'highlight' : ''}`} onClick={onSquareClicked}>{value}</button>
 }
